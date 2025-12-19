d9e9f23187f00dc57196b49b47d6106cc016018c
SumitRaj18 <sumitraj7162@gmail.com>
2025-12-19 21:02:43 +0530

    update

--- controllers/user.js
+++ controllers/user.js
@@ -40,8 +40,15 @@ const handleUserLogin=async(req,res)=>{
 }); 
     return res.status(200).json({msg:'Login Successfully',user:user })
 }
-const handleLogout=async(req,res)=>{
-    res.clearCookie("token");
-    return res.status(204).send("Logout Successfully")
-}
+const handleLogout = async (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0), 
+    secure: true,         
+    sameSite: "none",    
+    path: "/"            
+  });
+
+  return res.status(200).json({ msg: "Logout Successfully" });
+};
 module.exports={handleUserSignup,handleUserLogin,handleLogout};
